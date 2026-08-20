2b920706f34450eabad55f04fa52d5396d652154
emilymelo140-jpg <emilymelo140@gmail.com>
2026-08-20 12:04:57 -0300

    Add files via upload

--- meu.js
+++ meu.js
@@ -1,6 +1,9 @@
+const cepDigitado = document.getElementById("cep")
+const b = document.getElementById("bairro")
 async function buscar () {
+   
 const response = await fetch (`https://viacep.com.br/ws/${55014658}/json/`)
 let endereco = await response.json()
-
-console.log(endereco)
+b.value = await response.json()
+//console.log(endereco)
 }
